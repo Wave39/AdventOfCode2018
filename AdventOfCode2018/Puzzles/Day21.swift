96425f82b59c57497a097feb289bd8e9faf8e16c
Brian Prescott <bp@wave39.com>
2018-12-22 08:35:20 -0500

    Day 21 optimization attempts (unsuccessful)

--- AdventOfCode2018/Puzzles/Day21.swift
+++ AdventOfCode2018/Puzzles/Day21.swift
@@ -12,8 +12,33 @@ class Day21: NSObject {
 
     typealias Registers = [Int]
     
+    enum Opcode {
+        case unknown
+        case addr
+        case addi
+        case mulr
+        case muli
+        case banr
+        case bani
+        case borr
+        case bori
+        case setr
+        case seti
+        case gtir
+        case gtri
+        case gtrr
+        case eqir
+        case eqri
+        case eqrr
+    }
+    
+    let opcodeDict: Dictionary<String, Opcode> = [ "addr": .addr, "addi": .addi, "mulr": .mulr, "muli": .muli,
+                                                   "banr": .banr, "bani": .bani, "borr": .borr, "bori": .bori,
+                                                   "setr": .setr, "seti": .seti, "gtir": .gtir, "gtri": .gtri,
+                                                   "gtrr": .gtrr, "eqir": .eqir, "eqri": .eqri, "eqrr": .eqrr ]
+    
     class Command {
-        var opcode: String = ""
+        var opcode: Opcode = .unknown
         var a: Int = 0
         var b: Int = 0
         var c: Int = 0
@@ -36,7 +61,7 @@ class Day21: NSObject {
             } else {
                 let components = line.capturedGroups(withRegex: "(.*) (.*) (.*) (.*)", trimResults: true)
                 let cmd = Command()
-                cmd.opcode = components[0]
+                cmd.opcode = opcodeDict[components[0]]!
                 cmd.a = Int(components[1])!
                 cmd.b = Int(components[2])!
                 cmd.c = Int(components[3])!
@@ -49,37 +74,37 @@ class Day21: NSObject {
 
     public func runCommandString(command: Command, registers: Registers) -> Registers {
         var newRegisters = registers
-        if command.opcode == "addr" {
+        if command.opcode == .addr {
             newRegisters[command.c] = registers[command.a] + registers[command.b]
-        } else if command.opcode == "addi" {
+        } else if command.opcode == .addi {
             newRegisters[command.c] = registers[command.a] + command.b
-        } else if command.opcode == "mulr" {
+        } else if command.opcode == .mulr {
             newRegisters[command.c] = registers[command.a] * registers[command.b]
-        } else if command.opcode == "muli" {
+        } else if command.opcode == .muli {
             newRegisters[command.c] = registers[command.a] * command.b
-        } else if command.opcode == "banr" {
+        } else if command.opcode == .banr {
             newRegisters[command.c] = registers[command.a] & registers[command.b]
-        } else if command.opcode == "bani" {
+        } else if command.opcode == .bani {
             newRegisters[command.c] = registers[command.a] & command.b
-        } else if command.opcode == "borr" {
+        } else if command.opcode == .borr {
             newRegisters[command.c] = registers[command.a] | registers[command.b]
-        } else if command.opcode == "bori" {
+        } else if command.opcode == .bori {
             newRegisters[command.c] = registers[command.a] | command.b
-        } else if command.opcode == "setr" {
+        } else if command.opcode == .setr {
             newRegisters[command.c] = registers[command.a]
-        } else if command.opcode == "seti" {
+        } else if command.opcode == .seti {
             newRegisters[command.c] = command.a
-        } else if command.opcode == "gtir" {
+        } else if command.opcode == .gtir {
             newRegisters[command.c] = (command.a > registers[command.b] ? 1 : 0)
-        } else if command.opcode == "gtri" {
+        } else if command.opcode == .gtri {
             newRegisters[command.c] = (registers[command.a] > command.b ? 1 : 0)
-        } else if command.opcode == "gtrr" {
+        } else if command.opcode == .gtrr {
             newRegisters[command.c] = (registers[command.a] > registers[command.b] ? 1 : 0)
-        } else if command.opcode == "eqir" {
+        } else if command.opcode == .eqir {
             newRegisters[command.c] = (command.a == registers[command.b] ? 1 : 0)
-        } else if command.opcode == "eqri" {
+        } else if command.opcode == .eqri {
             newRegisters[command.c] = (registers[command.a] == command.b ? 1 : 0)
-        } else if command.opcode == "eqrr" {
+        } else if command.opcode == .eqrr {
             newRegisters[command.c] = (registers[command.a] == registers[command.b] ? 1 : 0)
         }
 
@@ -89,8 +114,8 @@ class Day21: NSObject {
     public func solve() {
         let puzzleInput = Day21PuzzleInput.puzzleInput
         
-        //let part1 = solvePart1(puzzleInput: puzzleInput)
-        //print ("Part 1 solution: \(part1)")
+        let part1 = solvePart1(puzzleInput: puzzleInput)
+        print ("Part 1 solution: \(part1)")
         let part2 = solvePart2(puzzleInput: puzzleInput)
         print ("Part 2 solution: \(part2)")
     }
@@ -124,14 +149,6 @@ class Day21: NSObject {
             if program.instructionPointer == 28 {
                 if registerSet.contains(program.registers[2]) {
                     return previous
-                }
-                
-                if registerSet.count > 0 {
-                    print("Count: \(registerSet.count), Last: \(registerSet.last!), Item: \(program.registers[2])")
-                }
-                
-                if registerSet.count > 17000 {
-                    print("Past the accepted answer");
                 }
                 
                 registerSet.append(program.registers[2])
